6798cafe9dd37fe4d0577d2ca69cc5bd94e1dc66
PallabRoy-Cy <pallabr8@gmail.com>
2021-12-12 01:36:14 +0600

    final tasks upload

---
+++ Task-API/storage/framework/views/6169b329b41983abc91c4c4e7812b23015c9d52f.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+	<h1 class="text-center">Add Product</h1>
+	<form method="POST" action="/store">
+        
+		<?php echo csrf_field(); ?>
+		<div class="mb-3">
+			<label><b>Name:</b></label>
+			<input type="text" name="name"  class="form-control">
+		</div>
+        
+		<div class="mb-3">
+			<label><b>Price:</b></label>
+			<input type="text" name="price"  class="form-control">
+		</div>
+        <div class="mb-3">
+			<label><b>Quntity:</b></label>
+			<input type="text" name="qty"  class="form-control">
+		</div>
+        <div class="mb-3">
+			<label><b>Description:</b></label>
+			<input type="text" name="description"  class="form-control">
+		</div>
+		<input type="submit" name="insert" value="Insert" class="btn btn-primary">
+	</form>
+</div>
+
+</body>
+</html><?php /**PATH C:\xampp\htdocs\delivery_boy\resources\views/create.blade.php ENDPATH**/ ?>
